06e0f01f0b98e476bc27094e9246da5ab87d8b5a
Eduard Deisling <edikdeisling@gmail.com>
2022-05-01 15:52:37 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="app">
+    App
+  </div>
+</template>
+
+<style scoped>
+  .app {
+    background: red;
+
+    &:hover {
+      background: green;
+    }
+
+    opacity: 0; /* Remove this line to get it works again */
+  }
+</style>
